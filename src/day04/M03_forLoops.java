e30ddb55ff3b581e309bf6b6ec436b4bbf8564e8
Ecandgn <160060211+Ecandgn@users.noreply.github.com>
2024-04-21 11:22:46 +0300

    M03_forLoops

--- src/day04/M03_forLoops.java
+++ src/day04/M03_forLoops.java
@@ -2,6 +2,12 @@ package day04;
 
 public class M03_forLoops {
     public static void main(String[] args) {
+        // 1 'den 100 E kadar olan sayılardan 3 ile tam bölünenleri yazdırın
 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
